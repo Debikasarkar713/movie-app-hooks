639d1ebe489badc282d3f37d556b15630a9db220
debika sarkar <debikasarkar713@gmail.com>
2019-10-10 14:30:06 -0400

    Add fetch to api

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../logo.svg";
 import "../App.css";
 import Header from "./Header";
@@ -7,22 +7,51 @@ import Search from "./Search";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=eebf486e";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=eebf486e`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Search search={search} />
+
+      <p className='App-intro'>Share a few movies</p>
+      <div className='movies'>
+        {loading && !errorMessage ? (
+          <span>...loading</span>
+        ) : errorMessage ? (
+          <div className='errorMessage'>{errorMessage}</div>
+        ) : (
+          movies.map((movie, i) => (
+            <Movie key={`${i}-${movie.title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
